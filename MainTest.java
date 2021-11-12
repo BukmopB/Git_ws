441b3bc8433fbc1c7104f3682e315088dc2f5f32
Victor Basov <bukmopbacob@gmail.com>
2021-11-12 17:54:06 +0300

    Rebase Testfun2

---
+++ MainTest.java
@@ -0,0 +1,21 @@
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+    String someString = "Hello, world!";
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testPrintInt () {
+        String actual = Main.printInt(50);
+        String expected = "50";
+        assertEquals(actual, expected);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
