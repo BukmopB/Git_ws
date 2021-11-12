b8d72327f8cc4f5e6a08e13c1c037475191e72b7
Victor Basov <bukmopbacob@gmail.com>
2021-11-12 16:52:29 +0300

    Test main method

---
+++ MainTest.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+    String someString = "Hello, world!";
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
