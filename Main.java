777225b109aa86db5feb89cdf245c941b3b7ab32
Victor Basov <bukmopbacob@gmail.com>
2021-11-12 16:15:03 +0300

    change Main

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(printString());
-    }
-
-    public static String printString() {
-        return "Hello, world";
+        ProjectController projectController = new ProjectController();
     }
 }
