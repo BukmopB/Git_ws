92bee6b1e444340fb7d1c254fcff3e149429e347
Victor Basov <bukmopbacob@gmail.com>
2021-11-12 16:36:49 +0300

    Print String fun 1 + add file

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         ProjectController projectController = new ProjectController();
+
+        System.out.println(projectController.string);
     }
 }
---
+++ ProjectController2.java
@@ -0,0 +1,7 @@
+public class ProjectController2 {
+    public String string = "Good by!";
+
+    public ProjectController2() {
+        System.out.println(string);
+    }
+}
