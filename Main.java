a90053ba5628be5cbda769f017010ccccab31122
Victor Basov <bukmopbacob@gmail.com>
2021-11-12 16:59:48 +0300

    Main print String

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         ProjectController projectController = new ProjectController();
-
         System.out.println(projectController.string);
+
+        System.out.println(printString());
+    }
+
+    public static String printString() {
+        return "Print String";
     }
 }
