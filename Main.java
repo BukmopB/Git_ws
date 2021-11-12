31dfb77dfabb5998638124398265f5b11f080c7d
Victor Basov <bukmopbacob@gmail.com>
2021-11-12 15:58:18 +0300

    Add *.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(printString());
+    }
+
+    public static String printString() {
+        return "Hello, world";
+    }
+}
